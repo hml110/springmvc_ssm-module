f7664e936f9e162895044fffcf4b2a1e9c021dec
Huminglong <2550721739@qq.com>
2022-10-17 11:21:02 +0800

    这是一个SSM基于XML配置的整合模板---maven下的父子模块拆分

---
+++ ssm_service/src/test/java/com/hml/service/UserServiceTest.java
@@ -0,0 +1,80 @@
+package com.hml.service;
+
+import com.github.pagehelper.PageInfo;
+import com.hml.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+
+/**
+ * @Description: service测试类
+ * @ClassName: UserServiceTest
+ * @Author: 25507
+ * @Date: 2022-08-13 21:12
+ * @Version: 1.0
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//注意此处用到了很多dao层的接口数据源，推荐解决的方案就是使用数据引入多个配置文件，或者在一个配置文件中写上所缺少的文件
+@ContextConfiguration(locations = {"classpath:applicationContext-service.xml","classpath:applicationContext-dao.xml"})
+public class UserServiceTest {
+    //注入接口
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testSave(){
+        User user = new User();
+        user.setUserName("Jock");
+        user.setPassword("root");
+        user.setRealName("Jockme");
+        user.setGender(1);
+        user.setBirthday(new Date(333333000000L));
+
+        userService.save(user);
+    }
+
+    @Test
+    public void testDelete(){
+        User user = new User();
+        userService.delete(3);
+    }
+
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setUuid(1);
+        user.setUserName("Jockme");
+        user.setPassword("root");
+        user.setRealName("JockIsMe");
+        user.setGender(1);
+        user.setBirthday(new Date(333333000000L));
+
+        userService.update(user);
+    }
+
+    @Test
+    public void testGet(){
+        User user = userService.getById(1);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testGetAll(){
+        PageInfo<User> all = userService.getAll(2, 2);
+        System.out.println(all);
+        System.out.println(all.getList().get(0));
+        System.out.println(all.getList().get(1));
+    }
+
+    @Test
+    public void testLogin(){
+        User user = userService.login("Jockme", "root");
+        System.out.println(user);
+    }
+
+}
